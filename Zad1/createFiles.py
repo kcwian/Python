e745bc4de7c9c40adb5a5280788576c6f7f2e5e0
kcwian <krzysztof.cwian@gmail.com>
2017-11-20 13:55:20 +0100

    Initial commit

---
+++ Zad1/createFiles.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+folderName = sys.argv[1]
+
+if not os.path.exists(folderName):
+   os.makedirs(folderName)
+
+for x in range(1,20):
+   f = open( "./" + folderName + "/test" + str(x), 'w')
+   f.write(str(x))
+   f.close()
+
+
